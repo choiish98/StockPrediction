19b767c2a496dca9c93ead4a2141851d7271f3cb
choiish98 <sojung7524@naver.com>
2021-05-09 18:31:29 +0900

    리사이클러뷰 텍스트로 예제 하나 생성

--- app/src/main/java/techtown/org/disclosure.java
+++ app/src/main/java/techtown/org/disclosure.java
@@ -3,6 +3,8 @@ package techtown.org;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
@@ -24,5 +26,19 @@ public class disclosure extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_disclosure);
+
+        // 리사이클러뷰에 표시할 데이터 리스트 생성.
+        ArrayList<String> list = new ArrayList<>();
+        for (int i=0; i<100; i++) {
+            list.add(String.format("TEXT %d", i)) ;
+        }
+
+        // 리사이클러뷰에 LinearLayoutManager 객체 지정.
+        RecyclerView recyclerView = findViewById(R.id.recyclerView) ;
+        recyclerView.setLayoutManager(new LinearLayoutManager(this)) ;
+
+        // 리사이클러뷰에 SimpleTextAdapter 객체 지정.
+        newsListAdapter adapter = new newsListAdapter(list) ;
+        recyclerView.setAdapter(adapter) ;
     }
 }
