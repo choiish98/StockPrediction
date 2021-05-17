bbd73b5288412096e2171684f84bad4b0cc271d1
lsh13 <captain-lee@hanmail.net>
2021-05-17 11:09:42 +0900

    create leee branch

--- app/src/main/java/techtown/org/Prediction.java
+++ app/src/main/java/techtown/org/Prediction.java
@@ -1,14 +1,142 @@
 package techtown.org;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+
 public class Prediction extends AppCompatActivity {
 
+    private LineChart chart;
+    private View view;
+
+    @SuppressLint("ResourceAsColor")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prediction);
+
+        //https://ddangeun.tistory.com/52
+        //real time chart
+        chart = (LineChart) view.findViewById(R.id.LineChart);
+        chart.setDrawGridBackground(true);
+        chart.setBackgroundColor(android.R.color.black);
+        chart.setGridBackgroundColor(android.R.color.black);
+
+// description text
+        chart.getDescription().setEnabled(true);
+        Description des = chart.getDescription();
+        des.setEnabled(true);
+        des.setText("Real-Time DATA");
+        des.setTextSize(15f);
+        des.setTextColor(android.R.color.white);
+
+// touch gestures (false-비활성화)
+        chart.setTouchEnabled(false);
+
+// scaling and dragging (false-비활성화)
+        chart.setDragEnabled(false);
+        chart.setScaleEnabled(false);
+
+//auto scale
+        chart.setAutoScaleMinMaxEnabled(true);
+
+// if disabled, scaling can be done on x- and y-axis separately
+        chart.setPinchZoom(false);
+
+//X축
+        chart.getXAxis().setDrawGridLines(true);
+        chart.getXAxis().setDrawAxisLine(false);
+
+        chart.getXAxis().setEnabled(true);
+        chart.getXAxis().setDrawGridLines(false);
+
+//Legend
+        Legend l = chart.getLegend();
+        l.setEnabled(true);
+        l.setFormSize(10f); // set the size of the legend forms/shapes
+        l.setTextSize(12f);
+        l.setTextColor(android.R.color.white);
+
+//Y축
+        YAxis leftAxis = chart.getAxisLeft();
+        leftAxis.setEnabled(true);
+        leftAxis.setTextColor(getResources().getColor(android.R.color.holo_blue_bright));
+        leftAxis.setDrawGridLines(true);
+        leftAxis.setGridColor(getResources().getColor(android.R.color.holo_blue_bright));
+
+        YAxis rightAxis = chart.getAxisRight();
+        rightAxis.setEnabled(false);
+
+
+// don't forget to refresh the drawing
+        chart.invalidate();
+
+        /*runOnUiThread(new Runnable() {
+            public void run() {
+                //Operate real time chart
+                addEntry(datanum);
+            }
+        });*/
     }
+    private void addEntry(double num) {
+
+        LineData data = chart.getData();
+
+        if (data == null) {
+            data = new LineData();
+            chart.setData(data);
+        }
+
+        ILineDataSet set = data.getDataSetByIndex(0);
+        // set.addEntry(...); // can be called as well
+
+        if (set == null) {
+            set = createSet();
+            data.addDataSet(set);
+        }
+
+
+
+        data.addEntry(new Entry((float)set.getEntryCount(), (float)num), 0);
+        data.notifyDataChanged();
+
+        // let the chart know it's data has changed
+        chart.notifyDataSetChanged();
+
+        chart.setVisibleXRangeMaximum(150);
+        // this automatically refreshes the chart (calls invalidate())
+        chart.moveViewTo(data.getEntryCount(), 50f, YAxis.AxisDependency.LEFT);
+
+    }
+
+    private LineDataSet createSet() {
+
+
+
+        LineDataSet set = new LineDataSet(null, "Real-time Line Data");
+        set.setLineWidth(1f);
+        set.setDrawValues(false);
+        set.setValueTextColor(getResources().getColor(android.R.color.white));
+        set.setColor(getResources().getColor(android.R.color.white));
+        set.setMode(LineDataSet.Mode.LINEAR);
+        set.setDrawCircles(false);
+        set.setHighLightColor(Color.rgb(190, 190, 190));
+
+        return set;
+    }
+
 }
